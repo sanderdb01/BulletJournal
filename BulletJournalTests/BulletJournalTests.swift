6b6872e7d5eff5c9d496178785c87b4bb42cebb2
David Sanders <dsanders@vectorform.com>
2025-10-01 14:57:04 -0400

    Initial Commit

---
+++ BulletJournalTests/BulletJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  BulletJournalTests.swift
+//  BulletJournalTests
+//
+//  Created by David Sanders on 10/1/25.
+//
+
+import Testing
+@testable import BulletJournal
+
+struct BulletJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
